6a672524f194881c58ec6ddc00c1e153ad770531
Ugyeong <66774144+Ugyeong@users.noreply.github.com>
2024-08-07 19:47:27 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 75.58 ms, Memory: 25.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+    string answer = "";
+    if(completion.size()==0) return participant[0];
+    sort(participant.begin(),participant.end());
+    sort(completion.begin(), completion.end());
+    for(int i=0; i<participant.size(); i++){
+        if(participant[i]!=completion[i]) {
+            answer = participant[i];
+            break;
+        }
+    }
+    return answer;
+}
