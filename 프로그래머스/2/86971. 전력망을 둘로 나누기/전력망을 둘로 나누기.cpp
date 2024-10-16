d0adbbfce9912cdbbc31f9e8bcaa4323eaca2231
Ugyeong <66774144+Ugyeong@users.noreply.github.com>
2024-10-17 04:13:16 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 1.26 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
@@ -0,0 +1,50 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <cmath>
+
+using namespace std;
+
+int search(vector<vector<int>> v){
+    vector<bool> visited(v.size()+1);
+    queue<int> q;
+    int count = 1;
+    int tmp;
+    
+    q.push(1);
+    visited[q.front()] = true;
+    
+    while(!q.empty()){
+        for(int i=0; i<v[q.front()].size(); i++){
+            tmp = v[q.front()][i];
+            if(!visited[tmp]) {
+                q.push(tmp);
+                visited[tmp] = true;
+                count++;
+            }
+        }
+        q.pop();
+    }
+    int count2 = v.size()-count-1;
+    return abs(count - count2);
+}
+
+int solution(int n, vector<vector<int>> wires) {
+    int answer = n;
+    int a,b;
+    
+    for(int i=0; i<wires.size(); i++){
+        vector<vector<int>> tmp(n+1); //위치에 따라서 오류남
+        for(int j=0; j<wires.size(); j++){
+            if(i==j) continue;
+            a = wires[j][0];
+            b = wires[j][1];
+            tmp[a].push_back(b);
+            tmp[b].push_back(a);
+        }
+        answer = min(answer, search(tmp));
+        tmp.clear();
+    }
+    
+    return answer;
+}
